1fa8e430897f8c3296a7519d562bff4e08b80b6a
taroosg <macchinetta.xyz@gmail.com>
2020-02-19 22:13:28 +0900

    update lesson17

--- code/lesson17.hs
+++ code/lesson17.hs
@@ -9,89 +9,114 @@ myMin = head . sort
 myMax :: Ord a => [a] -> a
 myMax = myLast . sort
 
+-- 全部条件と満たすとき
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll testFunc = (foldr (&&) True) . (map testFunc)
+
+-- どれか1つでも条件を満たすとき
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny testFunc = (foldl (||) False) . (map testFunc)
 
-instance Semigroup Integer where
-  (<>) x y = x + y  -- <>を+として定義
-
-data Color = Red | Yellow | Blue | Green | Purple | Orange | Brown deriving (Show, Eq)
-
-instance Semigroup Color where
-  (<>) Red Blue = Purple
-  (<>) Blue Red = Purple
-  (<>) Yellow Blue = Green
-  (<>) Blue Yellow = Green
-  (<>) Yellow Red = Orange
-  (<>) Red Yellow = Orange
-  -- これは結合律を満たさない
-  -- (<>) a b = if a == b
-  --   then a
-  --   else Brown
-  -- 結合的になるように書く
-  -- aとb療法が配列のどれかであるときはそれぞれの色になる
-  (<>) a b | a == b = a
-    | all (`elem` [Red, Blue, Purple]) [a, b] = Purple
-    | all (`elem` [Yellow, Blue, Green]) [a, b] = Green
-    | all (`elem` [Yellow, Red, Orange]) [a, b] = Orange
-    | otherwise = Brown
-
--- class Semigroup a => Monoid a where
---   identity :: a
-
-
--- 確率の計算
-type Events = [String]
-type Probs = [Double]
-
-data PTable = PTable Events Probs
-
-createPTable :: Events -> Probs -> PTable
-createPTable events probs = PTable events normalizedProbs
-  where totalProbs = sum probs
-        normalizedProbs = map (\x -> x/totalProbs) probs
-
-showPair :: String -> Double -> String
-showPair event prob = mconcat [event, "|", show prob, "\n"]
-
-instance Show PTable where
-  show (PTable events probs) = mconcat pairs
-    where pairs = zipWith showPair events probs
-
-cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
-cartCombine func l1 l2 = zipWith func newL1 cycledL2
-  where nToAdd = length l2
-        repeatedL1 = map (take nToAdd . repeat) l1
-        newL1 = mconcat repeatedL1
-        cycledL2 = cycle l2
-
-combineEvents :: Events -> Events -> Events
-combineEvents e1 e2 = cartCombine combiner e1 e2
-  where combiner = (\x y -> mconcat [x, "-", y])
-
-combineProbs :: Probs -> Probs -> Probs
-combineProbs p1 p2 = cartCombine (*) p1 p2
-
-instance Semigroup PTable where
-  -- 2つの確率テーブルが存在する場合を想定しているが，はじめに一つしか存在しない場合を決めておく
-  (<>) ptable1 (PTable [] []) = ptable1
-  (<>) (PTable [] []) ptable2 = ptable2
-  -- 2つの確率テーブルが両方とも存在する場合
-  (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
-    where newEvents = combineEvents e1 e2
-          newProbs = combineProbs p1 p2
-
-instance Monoid PTable where
-  -- 空である場合の要素の定義
-  mempty = PTable [] []
-  -- appendの処理
-  mappend = (<>)
-
-coin :: PTable
-coin = createPTable ["heads", "tails"] [0.5, 0.5]
-
-spinner :: PTable
-spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
-
 main = do
-  print (mconcat [coin, coin, coin])
+  print(myAll even [1,2,3])
+
+
+
+
+-- import Data.List
+
+-- myLast :: [a] -> a
+-- myLast = head . reverse
+
+-- myMin :: Ord a => [a] -> a
+-- myMin = head . sort
+
+-- myMax :: Ord a => [a] -> a
+-- myMax = myLast . sort
+
+-- myAny :: (a -> Bool) -> [a] -> Bool
+-- myAny testFunc = (foldl (||) False) . (map testFunc)
+
+-- instance Semigroup Integer where
+--   (<>) x y = x + y  -- <>を+として定義
+
+-- data Color = Red | Yellow | Blue | Green | Purple | Orange | Brown deriving (Show, Eq)
+
+-- instance Semigroup Color where
+--   (<>) Red Blue = Purple
+--   (<>) Blue Red = Purple
+--   (<>) Yellow Blue = Green
+--   (<>) Blue Yellow = Green
+--   (<>) Yellow Red = Orange
+--   (<>) Red Yellow = Orange
+--   -- これは結合律を満たさない
+--   -- (<>) a b = if a == b
+--   --   then a
+--   --   else Brown
+--   -- 結合的になるように書く
+--   -- aとb療法が配列のどれかであるときはそれぞれの色になる
+--   (<>) a b | a == b = a
+--     | all (`elem` [Red, Blue, Purple]) [a, b] = Purple
+--     | all (`elem` [Yellow, Blue, Green]) [a, b] = Green
+--     | all (`elem` [Yellow, Red, Orange]) [a, b] = Orange
+--     | otherwise = Brown
+
+-- -- class Semigroup a => Monoid a where
+-- --   identity :: a
+
+
+-- -- 確率の計算
+-- type Events = [String]
+-- type Probs = [Double]
+
+-- data PTable = PTable Events Probs
+
+-- createPTable :: Events -> Probs -> PTable
+-- createPTable events probs = PTable events normalizedProbs
+--   where totalProbs = sum probs
+--         normalizedProbs = map (\x -> x/totalProbs) probs
+
+-- showPair :: String -> Double -> String
+-- showPair event prob = mconcat [event, "|", show prob, "\n"]
+
+-- instance Show PTable where
+--   show (PTable events probs) = mconcat pairs
+--     where pairs = zipWith showPair events probs
+
+-- cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+-- cartCombine func l1 l2 = zipWith func newL1 cycledL2
+--   where nToAdd = length l2
+--         repeatedL1 = map (take nToAdd . repeat) l1
+--         newL1 = mconcat repeatedL1
+--         cycledL2 = cycle l2
+
+-- combineEvents :: Events -> Events -> Events
+-- combineEvents e1 e2 = cartCombine combiner e1 e2
+--   where combiner = (\x y -> mconcat [x, "-", y])
+
+-- combineProbs :: Probs -> Probs -> Probs
+-- combineProbs p1 p2 = cartCombine (*) p1 p2
+
+-- instance Semigroup PTable where
+--   -- 2つの確率テーブルが存在する場合を想定しているが，はじめに一つしか存在しない場合を決めておく
+--   (<>) ptable1 (PTable [] []) = ptable1
+--   (<>) (PTable [] []) ptable2 = ptable2
+--   -- 2つの確率テーブルが両方とも存在する場合
+--   (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
+--     where newEvents = combineEvents e1 e2
+--           newProbs = combineProbs p1 p2
+
+-- instance Monoid PTable where
+--   -- 空である場合の要素の定義
+--   mempty = PTable [] []
+--   -- appendの処理
+--   mappend = (<>)
+
+-- coin :: PTable
+-- coin = createPTable ["heads", "tails"] [0.5, 0.5]
+
+-- spinner :: PTable
+-- spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
+
+-- main = do
+--   print (mconcat [coin, coin, coin])
