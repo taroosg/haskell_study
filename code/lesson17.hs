948a9ca757030bb490c03d45199a683103746b39
taroosg <macchinetta.xyz@gmail.com>
2020-02-23 14:25:54 +0900

    update lesson17 done

--- code/lesson17.hs
+++ code/lesson17.hs
@@ -22,9 +22,12 @@ myAny testFunc = (foldl (||) False) . (map testFunc)
 instance Semigroup Integer where
   (<>) x y = x + y  -- <>という演算子を+として定義している
 
-data Color = Red | Yellow | Blue | Green | Purple | Orange | Brown deriving (Show ,Eq)
+data Color = Red | Yellow | Blue | Green | Purple | Orange | Brown | Clear deriving (Show ,Eq)
 instance Semigroup Color where
   -- Red <> Blueと記述するとPurpleを返す
+  -- 単位元としてClearを追加
+  (<>) Clear any = any
+  (<>) any Clear = any
   (<>) Red Blue = Purple
   (<>) Blue Red = Purple
   (<>) Yellow Blue = Green
@@ -39,27 +42,35 @@ instance Semigroup Color where
            | all (`elem` [Red, Yellow, Orange]) [a, b] = Orange
            | otherwise = Brown
 
+instance Monoid Color where
+  -- Clearが単位元
+  mempty = Clear
+  -- mappend関数を定義（Semigroupの演算を設定）
+  mappend c1 c2 = (<>) c1 c2
+
+
   -- monoidは入力と出力が同じでないといけない
   -- 出力が文字列になるのでNG
   -- mconcat ['a','b','c','d','e']
 
-type Events = [String]
-type Probs = [Double]
+data Events = Events [String]
+data Probs = Probs [Double]
 
 data PTable = PTable Events Probs
 
 createPTable :: Events -> Probs -> PTable
-createPTable events probs = PTable events normalizedProbs
+createPTable (Events events) (Probs probs) = PTable (Events events) (Probs normalizedProbs)
   where totalProbs = sum probs
         normalizedProbs = map (\x -> x/totalProbs) probs
 
 -- eventとprobsをいい感じの形（[event | prob \n]）に整形する
+-- 単純に文字列と数値を入力していい感じにするだけ
 showPair :: String -> Double -> String
-showPair event prob = mconcat [event, "|", show prob, "\n"]
+showPair events probs = mconcat [events, "|", show probs, "\n"]
 
 -- eventのリストと確率のリストをzipWithして全部つなげて（mconcat）表示
 instance Show PTable where
-  show (PTable events probs) = mconcat pairs
+  show (PTable (Events events) (Probs probs)) = mconcat pairs
     where pairs = zipWith showPair events probs
 
 -- createPTable ["heads","tails"] [0.5,0.5]するとheads|0.5 \n tails|0.5が返る
@@ -76,15 +87,30 @@ cartCombine func l1 l2 = zipWith func newL1 cycledL2
 
 -- 2つのイベントをいい感じにくっつける関数（heads-tailsの形）
 combineEvents :: Events -> Events -> Events
-combineEvents e1 e2 = cartCombine combiner e1 e2
+combineEvents (Events e1) (Events e2) = Events (cartCombine combiner e1 e2)
   where combiner = (\x y -> mconcat [x, "-", y])
+
+instance Semigroup Events where
+  (<>) = combineEvents
+
+instance Monoid Events where
+  mempty = Events []
+  mappend = (<>)
 
 -- 2つの確率をいい感じにくっつける関数（cartCombineのfuncに入れるといい感じになる）
 combineProbs :: Probs -> Probs -> Probs
-combineProbs p1 p2 = cartCombine (*) p1 p2
+combineProbs (Probs p1) (Probs p2) = Probs (cartCombine (*) p1 p2)
+
+instance Semigroup Probs where
+  (<>) = combineProbs
+
+instance Monoid Probs where
+  mempty = Probs []
+  mappend = (<>)
+
 
 instance Semigroup PTable where
-  (<>) ptable1 (PTable [] []) = ptable1 -- PTableが空の場合
+  (<>) ptable1 (PTable (Events []) (Probs [])) = ptable1 -- PTableが空の場合
   -- PTable型はStringのイベントとDoubleの確率から定義される
   -- 2つの確率テーブルを<>で演算するといい感じに組み合わさったテーブルになって出力される
   (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
@@ -95,17 +121,19 @@ instance Semigroup PTable where
 -- mappendには上で設定した<>を使用する
 -- Monoidはmempty（単位元：加算なら0，乗算なら1など）とmappend（やりたい演算）を定義すれば良い
 instance Monoid PTable where
-  mempty = PTable [] []
+  mempty = PTable (Events []) (Probs [])
   mappend = (<>)
 
 -- コインの確率テーブルを定義
 -- createPTable関数にイベントのリストと確率のリストを入力
+-- 入力するときはEvents型とProbs型を明示する
 coin :: PTable
-coin = createPTable ["heads", "tails"] [0.5, 0.5]
+coin = createPTable (Events ["heads", "tails"]) (Probs [0.5, 0.5])
 
 -- スピナーの確率テーブルを定義
+-- 入力するときはEvents型とProbs型を明示する
 spinner :: PTable
-spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
+spinner = createPTable (Events ["red", "blue", "green"]) (Probs [0.1, 0.2, 0.7])
 
 -- 下記のようにするとcoinとspinnerを組み合わせた確率テーブルができる
 -- coin <> spinner
@@ -114,7 +142,7 @@ spinner = createPTable ["red", "blue", "green"] [0.1, 0.2, 0.7]
 -- mconcat [coin, coin, coin]
 
 main = do
-  print(mconcat [spinner, spinner, spinner])
+  print(mconcat [coin, coin, spinner])
 
 -- import Data.List
 
