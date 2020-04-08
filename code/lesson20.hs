9c15ee0ac9d4b5f38e7e9ed1ac899ca66e664e63
taroosg <macchinetta.xyz@gmail.com>
2020-04-09 00:07:06 +0900

    update 20.3 done

--- code/lesson20.hs
+++ code/lesson20.hs
@@ -117,8 +117,69 @@ instance Monoid (TS a) where
   mempty = TS [] []
   mappend = (<>)
 
+-- ファイルの中身をリストに入れて全部まとめる関数
 tsAll :: TS Double
 tsAll = mconcat [ts1, ts2, ts3, ts4]
 
+-- 平均を求める関数
+-- 数値系の値の配列を平均して結果を返す
+mean :: (Real a) => [a] -> Double
+mean xs = total / count
+  where total = (realToFrac . sum) xs
+        count = (realToFrac . length) xs
+
+meanTS :: (Real a) => TS a -> Maybe Double
+meanTS (TS _ []) = Nothing
+-- 全部のvaluesがNothingだった場合．．．
+meanTS (TS times values) = if all (==Nothing) values
+  then Nothing
+  else Just avg
+    -- Justかどうかのテスト
+    where justVals = filter isJust values
+          -- 全部にjustValsを実行してパスしたものだけのリストを作成
+          cleanVals = map (\(Just x) -> x) justVals
+          -- 全部Justのリストの平均値
+          avg = mean cleanVals
+
+-- 比較関数の型定義
+type CompareFunc a = (a -> a -> a)
+-- TS型を比較する関数の型定義
+type TSCompareFunc a = ((Int, Maybe a) -> (Int, Maybe a) -> (Int, Maybe a))
+
+-- 比較関数を入力すると入力した関数で比較した結果を返す関数
+-- makeTSCompare min (3, Just 100) (4, Just 10)のように使える
+makeTSCompare :: Eq a => CompareFunc a -> TSCompareFunc a
+makeTSCompare func = newFunc
+  -- whereの中でパターンマッチング
+  -- 両方Nothingの場合
+  where newFunc (i1, Nothing) (i2, Nothing) = (i1, Nothing)
+        -- 片方Nothingの場合
+        newFunc (_, Nothing) (i, val) = (i, val)
+        newFunc (i, val) (_, Nothing) = (i, val)
+        -- 両方valが存在するとき
+        -- 両方を比較して結果によってどちらかを返す
+        newFunc (i1, Just val1) (i2, Just val2) = if (func val1 val2) == val1
+          then (i1, Just val1)
+          else (i2, Just val2)
+
+-- TS型データに比較関数を適用する関数
+-- 比較関数とTSデータを入力すると比較関数に引っかかったタプルを返す
+compareTS :: Eq a => (a -> a -> a) -> TS a -> Maybe (Int, Maybe a)
+compareTS func (TS [] []) = Nothing
+compareTS func (TS times values) = if all (==Nothing) values
+  then Nothing
+  else Just best
+    where pairs = zip times values
+          -- 順番に(o, Nothing)と比較する
+          best = foldl (makeTSCompare func) (0, Nothing) pairs
+
+-- compareTSにminを入力
+minTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+minTS = compareTS min
+
+-- compareTSにmaxを入力
+maxTS :: Ord a => TS a -> Maybe (Int, Maybe a)
+maxTS = compareTS max
+
 main = do
-  print (tsAll)
+  print (maxTS tsAll)
